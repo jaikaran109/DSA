59f7ca016a0b8790b26ec813fdf539701bbdb881
jaikaran109 <jaik05581@gmail.com>
2026-01-05 21:56:04 +0530

    Create HalfPyramid.java

---
+++ Pattern/HalfPyramid.java
@@ -0,0 +1,25 @@
+package Pattern;
+
+//
+//* 
+//* * 
+//* * * 
+//* * * * 
+
+
+
+import java.util.*;
+public class HalfPyramid {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int n = input.nextInt();
+		for(int i = 0 ; i < n ; i++) {
+			for(int j = 0 ; j <= i ; j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+
+}
