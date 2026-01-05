63e76437371e515c221aeb951d02862517df96bb
jaikaran109 <jaik05581@gmail.com>
2026-01-05 22:05:02 +0530

    Create SolidRhombus.java

---
+++ Pattern/SolidRhombus.java
@@ -0,0 +1,30 @@
+package Pattern;
+
+
+//    * * * * * 
+//   * * * * * 
+//  * * * * * 
+// * * * * * 
+//* * * * * 
+
+import java.util.Scanner;
+
+public class Solid_Rhombus {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int n = input.nextInt();
+		for(int i = 0 ; i < n ; i++) {
+			for(int j = n - 1; j > i ; j--) {
+				System.out.print("  ");
+			}
+			for(int j = 0 ; j < n ; j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+}
+
+	}
+
+
