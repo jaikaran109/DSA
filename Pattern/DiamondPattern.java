4f098e60fd25c4098a5eec9acb23c12653472f6f
jaikaran109 <jaik05581@gmail.com>
2026-01-05 21:54:06 +0530

    Create DiamondPattern.java

---
+++ Pattern/DiamondPattern.java
@@ -0,0 +1,44 @@
+package Pattern;
+
+//
+//        * 
+//      * * * 
+//    * * * * * 
+//  * * * * * * * 
+//* * * * * * * * * 
+//  * * * * * * * 
+//    * * * * * 
+//      * * * 
+//        * 
+  
+
+
+import java.util.*;
+public class DiamondPattern {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int n = input.nextInt();
+		for(int i = 1 ; i <= n ; i++) {
+			for(int j = n ; j > i ; j--) {
+				System.out.print("  ");
+			}
+			for(int j = 1 ; j <= 2*i - 1 ; j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+		for(int i = 1 ; i <= n ; i++) {
+			for(int j = 1 ; j <= i ; j++) {
+				System.out.print("  ");
+			}
+			for(int j = 1 ; j <= 2*(n-i) - 1 ; j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+		
+
+	}
+
+}
