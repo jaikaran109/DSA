085f97aadea6ccee9bb22e90294ed67b2c1986dc
jaikaran109 <jaik05581@gmail.com>
2026-01-06 14:39:38 +0530

    Create Case.java

---
+++ String/Case.java
@@ -0,0 +1,15 @@
+package String;
+
+import java.util.*;
+
+public class upperAndLower {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		String str =input.next();
+		System.out.println(str.toLowerCase());
+		System.out.print(str.toUpperCase());
+
+	}
+
+}
