079f0806f0cdea1feec4d5a2d771ca98a3f9e5f5
jaikaran109 <jaik05581@gmail.com>
2026-01-06 14:32:21 +0530

    Create Parse_Int.java

---
+++ String/Parse_Int.java
@@ -0,0 +1,27 @@
+package String;
+
+public class ParseInt {
+
+	public static void main(String[] args) {
+		String str = "123";
+		int num = Integer.parseInt(str);
+		System.out.print(num);
+
+	}
+
+}
+
+
+
+
+//number to string
+//public class Strings {
+//	   public static void main(String args[]) {
+//	      int number = 123;
+//	      String str = Integer.toString(number);
+//	      System.out.println(str.length());
+//	      
+//	      
+//	   }
+//	}
+
