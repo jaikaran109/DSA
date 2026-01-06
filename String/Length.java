778f0d718172db2f61cf166ae40362cb163002a0
jaikaran109 <jaik05581@gmail.com>
2026-01-06 14:37:55 +0530

    Create Length.java

---
+++ String/Length.java
@@ -0,0 +1,14 @@
+package String;
+
+import java.util.*;
+
+public class String_Len {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		String str = input.next();
+		System.out.print(str.length());
+
+	}
+
+}
