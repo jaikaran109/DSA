64970e40d40d9a7674f47862e6a6750f2e4b5991
jaikaran109 <jaik05581@gmail.com>
2026-01-14 19:22:06 +0530

    Create InfiniteLoop.java

---
+++ Methods/InfiniteLoop.java
@@ -0,0 +1,14 @@
+package Methods;
+
+
+// Write an infinite loop using do while condition.
+
+
+public class InfiniteLoop {
+    public static void main(String[] args) {
+        do {
+            System.out.println("Infinite Loop");
+        } while (true);
+    }
+}
+//don't Run
