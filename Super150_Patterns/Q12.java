50c0b812b578b22c95a7334757a161c3d760e6c0
jaikaran109 <jaik05581@gmail.com>
2026-01-18 17:22:59 +0530

    Create Q12.java

---
+++ Super150_Patterns/Q12.java
@@ -0,0 +1,41 @@
+// Ques 12: 
+// n = 5 
+ 
+//         *  
+//       * ! *  
+//     * ! * ! *  
+//   * ! * ! * ! *  
+// * ! * ! * ! * ! * 
+
+
+
+import java.util.Scanner;
+public class Q12 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int star = 1;
+        int space = n-1;
+        int i = 1;
+        while(i<=n){
+            int j = 1;
+            while(j<=space){
+                System.out.print("  ");
+                j++;
+            }
+            int k = 1;
+            while(k<=star){
+                if(k%2==0){
+                    System.out.print("! ");
+                }else{
+                    System.out.print("* ");
+                }
+                k++;
+            }
+            System.out.println();
+            i++;
+            space--;
+            star+=2;
+        }
+    }
+}
