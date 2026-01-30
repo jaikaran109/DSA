644361e2c988b0b67b87573448b06cc9e319d4b7
jaikaran109 <jaik05581@gmail.com>
2026-01-30 23:43:01 +0530

    Create Sphere.java

---
+++ Conditionals/Sphere.java
@@ -0,0 +1,10 @@
+// Volume Of Sphere
+import java.util.*;
+class Main Sphere{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double r = sc.nextDouble();
+        System.out.println("Volume of Sphere = " +
+                (4.0/3) * Math.PI * r * r * r);
+    }
+}
