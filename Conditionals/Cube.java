e2d5b839e68c0e71954d55cd786f4585fbd3d0d4
jaikaran109 <jaik05581@gmail.com>
2026-01-30 23:56:16 +0530

    Create Cube.java

---
+++ Conditionals/Cube.java
@@ -0,0 +1,9 @@
+// Total Surface Area Of Cube
+import java.util.*;
+class Main Cube{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double side = sc.nextDouble();
+        System.out.println("Total Surface Area of Cube = " + 6 * side * side);
+    }
+}
