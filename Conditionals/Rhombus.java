e9880001b2688e17678a2e84fe4218d5e0c198ab
jaikaran109 <jaik05581@gmail.com>
2026-01-30 23:33:26 +0530

    Update Rhombus.java

--- Conditionals/Rhombus.java
+++ Conditionals/Rhombus.java
@@ -9,7 +9,7 @@ public class Rhombus_Area {
         Scanner input = new Scanner(System.in);
 
         System.out.print("Enter diagonal 1: ");
-        double d1 = input.nextDouble();
+        double d1 = input.nextDouble(); // diagonals
 
         System.out.print("Enter diagonal 2: ");
         double d2 = input.nextDouble();
@@ -17,5 +17,9 @@ public class Rhombus_Area {
         double area = 0.5 * d1 * d2;
 
         System.out.println("Area of Rhombus = " + area);
+
+
+        double side = sc.nextDouble();
+        System.out.println("Perimeter of Rhombus = " + 4 * side);
     }
 }
