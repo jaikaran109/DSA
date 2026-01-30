2665c63daf9ad57d06f75ede4c287327c6830697
jaikaran109 <jaik05581@gmail.com>
2026-01-31 01:22:23 +0530

    Create Palindrome.java

---
+++ Conditionals/Palindrome.java
@@ -0,0 +1,18 @@
+// Find if a number is palindrome or not
+import java.util.*;
+
+public class PalindromeNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int temp = n, rev = 0;
+        while(temp > 0){
+            rev = rev * 10 + temp % 10;
+            temp /= 10;
+        }
+
+        if(rev == n) System.out.println("Palindrome");
+        else System.out.println("Not Palindrome");
+    }
+}
