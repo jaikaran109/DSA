d07ab37acaadf124107b77cd232d3dc017c33077
jaikaran109 <jaik05581@gmail.com>
2026-01-30 23:35:24 +0530

    Create Cylinder.java

---
+++ Conditionals/Cylinder.java
@@ -0,0 +1,10 @@
+// Volume Of Cylinder
+import java.util.*;
+class Main Cylinder{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double r = sc.nextDouble();
+        double h = sc.nextDouble();
+        System.out.println("Volume of Cylinder = " + Math.PI * r * r * h);
+    }
+}
