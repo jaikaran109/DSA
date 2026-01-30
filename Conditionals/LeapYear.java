7a49ad3b6e9ab1915cbb9bb5cb40fcd09ef38d08
jaikaran109 <jaik05581@gmail.com>
2026-01-31 01:24:13 +0530

    Create LeapYear.java

---
+++ Conditionals/LeapYear.java
@@ -0,0 +1,14 @@
+// Check Leap Year Or Not
+import java.util.*;
+
+public class LeapYear {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int y = sc.nextInt();
+
+        if((y % 4 == 0 && y % 100 != 0) || y % 400 == 0)
+            System.out.println("Leap Year");
+        else
+            System.out.println("Not Leap Year");
+    }
+}
