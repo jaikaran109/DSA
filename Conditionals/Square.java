40525e2ff5711381fe34cc75bb92ba84f1be0dae
jaikaran109 <jaik05581@gmail.com>
2026-01-30 23:29:17 +0530

    Create Square.java

---
+++ Conditionals/Square.java
@@ -0,0 +1,9 @@
+// Perimeter Of Square
+import java.util.*;
+class Main Square{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double side = sc.nextDouble();
+        System.out.println("Perimeter of Square = " + 4 * side);
+    }
+}
