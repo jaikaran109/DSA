e19aa0922a28a868466bc9494c0ae742213782f2
jaikaran109 <jaik05581@gmail.com>
2026-02-01 19:00:08 +0530

    Implement solution for finding missing numbers

---
+++ Sorting/CC_LC_Q448.java
@@ -0,0 +1,61 @@
+package Sorting;
+
+
+//Given an array nums of
+//n integers where nums [i] is in the range
+//[1, n], return an array of all the integers in the range ([1, n] that
+//do not appear in nums
+//
+//Example 1:
+//
+//Input: nums = [4,3,2,7,8,2,3,1]
+//Output: [5,6]
+//
+//Example 2:
+//
+//Input: nums = [1,1]
+//Output: [2]
+		
+		
+import java.util.*;
+public class CC_LC_Q448 {
+
+	public static List<Integer> findDisappearedNumbers(int[] nums) {
+        int i = 0;
+
+        while (i < nums.length) {
+            int correctIndex = nums[i] - 1;
+
+            if (nums[i] != nums[correctIndex]) {
+                int temp = nums[correctIndex];
+                nums[correctIndex] = nums[i];
+                nums[i] = temp;
+            } else {
+                i++;
+            }
+        }
+
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != j + 1) {
+                list.add(j + 1);
+            }
+        }
+        return list;
+    }
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        List<Integer> result = findDisappearedNumbers(nums);
+        System.out.println(result);
+	}
+
+}
