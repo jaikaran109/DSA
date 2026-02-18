9fade7729cacfd9ed2a3f8e6176c182b9255107b
jaikaran109 <jaik05581@gmail.com>
2026-02-18 10:25:15 +0530

    Update CheckSorting.java

--- Array/CheckSorting.java
+++ Array/CheckSorting.java
@@ -3,7 +3,7 @@ package Array;
 
 //Take an array of numbers as input and check if it is an array sorted in ascending order.
 //Eg : { 1, 2, 4, 7 } is sorted in ascending order.
-//       {3, 4, 6, 2} is not sorted in ascending order.
+//      {3, 4, 6, 2} is not sorted in ascending order.
 
 
 import java.util.*;
