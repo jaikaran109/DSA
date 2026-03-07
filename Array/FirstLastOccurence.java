61a80b7bec3fd9d794b81a08437cb1efb2d5917a
jaikaran109 <jaik05581@gmail.com>
2026-03-07 13:32:07 +0530

    Create FirstLastOccurence.java

---
+++ Array/FirstLastOccurence.java
@@ -0,0 +1,85 @@
+package Exam_4thSem;
+
+
+//Find First and Last Position of Element in Sorted Array.
+//Given an array of integers nums sorted in non-decreasing order, find
+//the starting and ending position of a given target value.
+//If target is not found in the array, return [-1, -1].
+//You must write an algorithm with O(log n) runtime complexity.
+//Example 1:
+//Input: nums = [5,7,7,8,8,10], target = 8
+//Output: [3,4]
+//Example 2:
+//Input: nums = [5,7,7,8,8,10], target = 6
+//Output: [-1,-1]
+//
+//Example 3:
+//Input: nums = [], target = 0
+//Output: [-1,-1]
+//Constraints:
+//
+//        0 <= nums.length <= 105
+//        -109 <= nums[i] <= 109
+//nums is a non-decreasing array.
+//        -109 <= target <= 109
+
+
+import java.util.*;
+public class FirstLastOccurence {
+    static int firstOcc(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        int ans = -1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target) {
+                ans = mid;
+                high = mid - 1;
+            } else if (nums[mid] > target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    static int lastOcc(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        int ans = -1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target) {
+                ans = mid;
+                low = mid + 1;
+            } else if (nums[mid] > target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int target = sc.nextInt();
+        int first = firstOcc(arr, target);
+        int last = lastOcc(arr, target);
+        System.out.println(first + " " + last);
+    }
+}
+
+
+
+
+// Time : O(log n)
+
+
+// Time : O(log n)
