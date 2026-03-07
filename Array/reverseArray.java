7bfad77037d4bea7022e9d841f299b5025acf629
jaikaran109 <jaik05581@gmail.com>
2026-03-07 13:51:30 +0530

    Update reverseArray.java

--- Array/reverseArray.java
+++ Array/reverseArray.java
@@ -46,3 +46,37 @@ public class ReverseArray {
 //Space Complexity -- O(1)
 //Time Complexity  -- O(n)
 //Space Complexity -- O(1)
+
+
+
+
+
+
+//Reverse an array
+
+
+// import java.util.*;
+// public class reverse {
+// 	static int[] rev(int[] nums) {
+// 		int low = 0;
+// 		int high = nums.length - 1;
+// 		while(low < high) {
+// 			int temp = nums[low];
+// 			nums[low] = nums[high];
+// 			nums[high] = temp;
+// 			low++;
+// 			high--;
+// 		}
+// 		return nums;
+// 	}
+// 	public static void main(String[] args) {
+// 		Scanner input = new Scanner(System.in);
+// 		int size = input.nextInt();
+// 		int[] arr = new int[size];
+// 		for(int i = 0 ; i < size ; i++) {
+// 			arr[i] = input.nextInt();
+// 		}
+// 		System.out.print(Arrays.toString(rev(arr)));
+// 	}
+
+// }
