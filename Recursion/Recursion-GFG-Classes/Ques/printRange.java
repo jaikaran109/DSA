c573f34971ad4078e2544112c10a3848958ca05f
jaikaran109 <jaik05581@gmail.com>
2026-08-04 16:23:30 +0530

    Add files via upload

---
+++ Recursion/Recursion-GFG-Classes/Ques/printRange.java
@@ -0,0 +1,16 @@
+package Recursion.Raghav;
+
+import java.util.*;
+public class printRange {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int start = input.nextInt();
+        int end = input.nextInt();
+        print(start,end);
+    }
+    static void print(int x , int n){
+        if(x > n) return;
+        System.out.println(x);
+        print(x+1,n);
+    }
+}
