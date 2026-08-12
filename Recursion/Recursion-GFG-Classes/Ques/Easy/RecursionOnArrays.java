1bbc6dc4c72ed56a176055be21a7ed4a4f2ad298
jaikaran109 <jaik05581@gmail.com>
2026-08-13 01:29:59 +0530

    Update RecursionOnArrays.java

--- Recursion/Recursion-GFG-Classes/Ques/Easy/RecursionOnArrays.java
+++ Recursion/Recursion-GFG-Classes/Ques/Easy/RecursionOnArrays.java
@@ -1,3 +1,4 @@
+package Recursion.Raghav;
 import java.util.*;
 public class RecursionOnArrays {
     public static void main(String[] args) {
@@ -8,7 +9,19 @@ public class RecursionOnArrays {
             arr[i] = input.nextInt();
         }
         printArray(arr,0);
+
+
+        System.out.println("Check present Or Not" + check(arr,7,0));
+
+
     }
+
+    static boolean check(int[] arr , int target , int idx){
+        if(idx == arr.length) return false;
+        if(arr[idx] == target) return true;
+        return check(arr,target,idx+1);
+    }
+
     static void printArray(int[] arr , int idx){
         if(idx == arr.length) return;
         System.out.print(arr[idx] + " ");
